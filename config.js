173dbd71e35aef0771f9e7aeb7203eba1031b55b
Ken_Livesey <plantdink@gmail.com>
2022-05-02 16:09:57 +1000

    edit endpoints for deployment

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // client side only
 export const endpoint = `http://localhost:3000/api/graphql`;
-export const prodEndpoint = `https://afm-backend.herokuapp.com/`;
+export const prodEndpoint = `https://afm-backend.herokuapp.com:3000/api/graphql`;
 export const perPage = 6;
